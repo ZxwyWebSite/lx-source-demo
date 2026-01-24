9b93894f1c9f59113144c5e0054562bf082059e9
Zxwy <112370739+ZxwyWebSite@users.noreply.github.com>
2026-01-24 11:33:54 +0800

    2026-01-24

--- build.sh
+++ build.sh
@@ -15,16 +15,27 @@ RELEASE=$(curl -s "https://github.com/$OWNER/$REPO/releases")
 # 使用正则表达式提取第一个 TAG
 VERSION=$(echo "$RELEASE" | grep -oP '/tree/\K[^"]+' | head -n 1)
 
-# 拼接下载链接
-if [[ -n "$VERSION" ]]; then
-  FILENAME="lxsrcs-$VERSION-linux-amd64v3-gnu.zip"
-  DOWNLINK="https://github.com/$OWNER/$REPO/releases/download/$VERSION/$FILENAME"
-  echo "下载链接: $DOWNLINK"
-
-  # 下载文件并解压
-  wget -O "$FILENAME" "$DOWNLINK" && unzip "$FILENAME"
-  echo "文件下载完毕！"
-else
+if [[ -z "$VERSION" ]]; then
   echo "没有找到符合条件的下载链接。"
   exit 1
 fi
+
+# 拼接文件名与下载链接
+FILENAME="lxsrcs-$VERSION-linux-amd64v3-gnu.zip"
+DOWNLINK="https://github.com/$OWNER/$REPO/releases/download/$VERSION/$FILENAME"
+
+echo "下载链接: $DOWNLINK"
+
+# 下载文件
+if ! wget -O "$FILENAME" "$DOWNLINK"; then
+  echo "下载失败！"
+  exit 1
+fi
+
+# 解压文件
+if ! unzip "$FILENAME"; then
+  echo "解压失败！"
+  exit 1
+fi
+
+echo "文件下载完毕！"
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 演示站 https://lxsrcs.onrender.com
 
 由于海外部署，仅支持 `WY` / `KW` / `KG` 平台演示，且无法解析付费资源。
+
+```sh
+# 启动命令
+cat build.sh start.sh | bash
+```
